3a33eb449cc9c71eaefc814a23a3aa7643ea45eb
MaxMartinez2006 <memartionez2004@gmial.com>
2024-09-10 18:44:43 -0600

    Agregue un arreglo

--- prueba1/src/main/java/org/ogcv/Main.java
+++ prueba1/src/main/java/org/ogcv/Main.java
@@ -1,17 +1,16 @@
 package org.ogcv;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[]args){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int [] ages = new int[10]:
+        ages [0]= 15;
+        ages [1]= 25;
+        ages [2]= 33;
+        ages [3]= 70;
+        ages [4]= 27;
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
         }
     }
 }
